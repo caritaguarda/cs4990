5ee29e84d246de3692e72b5d84c81bac2778979b
caritaguarda <cabraha2@dmail.dixie.edu>
2015-09-09 17:26:05 -0600

    work from wed 9-2

--- portfolio/casestudy/views.py
+++ portfolio/casestudy/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import CaseStudy, Item
+
+
+class IndexView(generic.ListView):
+    template_name = 'casestudy/index.html'
+    model = CaseStudy
+
+
+class DetailView(generic.DetailView):
+    model = CaseStudy
+    template_name = 'casestudy/detail.html'
+
+
+
+
+
