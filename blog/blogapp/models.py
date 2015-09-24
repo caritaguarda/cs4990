38a029d5379891bc55935db85353e32fa227c6e4
Carrie Guarda <cabraha2@dmail.dixie.edu>
2015-09-23 22:17:21 -0600

    completed blog assignment. comment form there but not working

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -2,22 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    body = models.TextField()
     title = models.CharField(max_length=200)
+    body = models.TextField()
     picture = models.ImageField(upload_to="pics/%Y/%m/%d", blank=True, null=True)
     pub_date = models.DateTimeField()
+    
+    category = models.ManyToManyField('Category', blank=True)
     keywords = models.CharField(max_length=200, blank=True, null=True)
-
-    comment = models.ManyToManyField('Comment')
-    category = models.ManyToManyField('Category')
-
+    
+   
     def __unicode__(self):
         return self.title
+
 
 class Comment(models.Model):
     person = models.CharField(max_length=200)
     comment_text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post)
+    #Foreign key will create a comment_set in the post class
 
     def __unicode__(self):
         return "%s - %s" % (self.person, self.comment_text[:50])
@@ -28,3 +31,5 @@ class Category(models.Model):
     def __unicode__(self):
         return self.category_name
 
+    class Meta: 
+        verbose_name_plural = "categories"
--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.views.generic import FormView
+from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .models import Post, Comment, Category
+from .forms import CommentForm
+
+class IndexView(generic.ListView):
+    template_name = 'blogapp/index.html'
+    model = Post
+    
+
+    def get_queryset(self):
+        #in order to only show the most recent 5 posts:
+        return Post.objects.order_by('-pub_date')[:10]
+
+class CategoryListView(generic.ListView):
+    template_name = 'blogapp/category.html'
+    model = Category
+    
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    success_url = reverse_lazy('blogapp:commentsuccess')
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        context["form"] = CommentForm
+        return context
+        
+    def form_valid(self, form):
+        #add a comment
+
+        comment = Comment()
+        comment.person = form.cleaned_data['name']
+        comment.comment_text = form.cleaned_data['comment']
+        comment.post = form.cleaned_data['post']
+        comment.save()
+        
+        return super(PostDetailView, self).form_valid(form)
+
+class CategoryDetailView(generic.DetailView):
+    model = Category
+    template_name = 'blogapp/categorydetail.html'
+
+class CommentFormView(FormView):
+    form_class = CommentForm
+    template_name = 'blogapp/commentform.html'
+    success_url = reverse_lazy('blogapp:commentsuccess')
+    
+
+    
+    
+    #def get_context_data(self):
+        #context = super(PostDetailView,self).get_context_data()
+        #context.update({"comment_list": self.get_object().comment_set.all()})
+        #context.update({"comment_list": Comment.objects.filter(post__pk = self.kwargs.get('pk'))})
+        #return context
